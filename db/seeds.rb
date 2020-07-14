03aef7b9ac436e6b0eb3c53b8964feb0b2d3045f
Igor Batista <igor.batista@gmail.com>
2020-07-14 16:24:12 -0300

    seed fill payables

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Payment.create(description: 'Débito',          nature: 'Entrada', signal: '+')
+Payment.create(description: 'Boleto',          nature: 'Saída', signal: '-')
+Payment.create(description: 'Financiamento',   nature: 'Saída', signal: '-')
+Payment.create(description: 'Crédito',         nature: 'Entrada', signal: '+')
+Payment.create(description: "Recebimento Empréstimo\t", nature: 'Entrada', signal: '+')
+Payment.create(description: 'Vendas',          nature: 'Entrada', signal: '+')
+Payment.create(description: 'Recebimento TED', nature: 'Entrada', signal: '+')
+Payment.create(description: 'Recebimento DOC', nature: 'Entrada', signal: '+')
+Payment.create(description: 'Aluguel',         nature: 'Saída',   signal: '-')
