0a2d082ff1959465a10028865bf288e6cbcadb3e
Igor Batista <igor.batista@gmail.com>
2020-07-14 18:12:51 -0300

    adjuste read cnab

--- app/services/cnab.rb
+++ app/services/cnab.rb
@@ -5,19 +5,20 @@ module Cnab
     end
     
     def call
-      @file = File.open(@upload_file, 'r')
+      @file = File.open(@upload_file.tempfile.path, 'r')
+      puts "lendo arquivo texto\n"
       @file.each do |line|
-        owner = line[48..61].strip
-        store_str = line[62..81].strip
+        owner = line[48..61].strip()
+        store_str = line[62..81].strip()
         store = Store.create_with(name: store_str, owner: owner).find_or_create_by(name: store_str, owner: owner)
         payment = Payment.find(line[0])
         negotiation = payment.negotiations.new
         negotiation.store_id   = store.id
-        negotiation.date       = line[1..8].strftime('%Y/%m/%d')
+        negotiation.date       = line[1..8].to_date.strftime('%Y/%m/%d')
         negotiation.price      = line[9..18].to_f / 100
         negotiation.cpf        = line[19..29]
         negotiation.card       = line[30..41]
-        negotiation.time       = line[42..47].strftime('%H:%M')
+        negotiation.time       = Time.parse("#{line[42..47][0..1]}:#{line[42..47][2..3]}:#{line[42..47][4..5]}")
         negotiation.save!
       end
     end
