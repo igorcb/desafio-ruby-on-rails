d2f6d670dba1fe719a21a999f39f9f432466e38c
Igor Batista <igor.batista@gmail.com>
2020-07-17 14:10:13 -0300

    config login devise and gem rails-controller-testing

--- Gemfile
+++ Gemfile
@@ -55,6 +55,7 @@ group :development do
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
   gem 'rspec-rails'
+  gem 'rails-controller-testing', '~> 1.0', '>= 1.0.5'
 end
 
 group :test do
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../config/environment', __dir__)
 # Prevent database truncation if the environment is production
-abort("The Rails environment is running in production mode!") if Rails.env.production?
+abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'rspec/rails'
 require 'database_cleaner/active_record'
 # Add additional requires below this line. Rails is not loaded until this point!
@@ -32,6 +32,8 @@ rescue ActiveRecord::PendingMigrationError => e
   exit 1
 end
 RSpec.configure do |config|
+  config.include Devise::Test::ControllerHelpers, type: :controller
+  config.extend ControllerMacros, type: :controller
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
